3b3e1e9fe584dfc3869495b3f02ff04db7560839
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-29 18:17:51 +0900

    typo: key_id -> keyid

--- app/services/gurumes/search_service.rb
+++ app/services/gurumes/search_service.rb
@@ -7,7 +7,7 @@ module Gurumes
     end
 
     def run
-      uri = URI.parse URI.encode("https://api.gnavi.co.jp/RestSearchAPI/v3/?key_id=#{ENV['GURUNAVI_API']}&address=#{@area}&freeword=#{@food}&hit_per_page=#{@hit}")
+      uri = URI.parse URI.encode("https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=#{ENV['GURUNAVI_API']}&address=#{@area}&freeword=#{@food}&hit_per_page=#{@hit}")
       response = JSON.load(Net::HTTP.get(uri))
       response['rest']
     end
