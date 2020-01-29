e1c633a34165c929926445906e40055530bdfe9b
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-29 17:57:50 +0900

    generated controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  root 'homes#index'
+  root to: 'homes#index'
+
+  namespace :api, { format: :json } do
+    namespace :v1 do
+      resources :searches, only: [:index]
+    end
+  end
 end
